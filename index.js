15b1983ba26eb755a02dd164eb086c523e728941
Axel Bock <axel.bock@dilax.com>
2017-10-25 16:12:50 +0200

    main method - "node start"

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var rect = {
     area: (x, y) => (x * y)
 };
 
-
 function solveRect(l, b) {
-    console.log("solving for rect with l = " + l + " and b = " + b)
-    if (l <= 0 || b <= 0) {
+    console.log("solving for rect with l=" + l + " and b=" + b);
 
+    if (l <= 0 || b <= 0) {
+        console.log("blabla 0");
+    } else {
+        console.log("area=" + rect.area(l, b));
+        console.log("perimeter=" + rect.perimeter(l, b));
     }
 }
+
+solveRect(2, 3);
+solveRect(3, 5);
+solveRect(3, 0);
